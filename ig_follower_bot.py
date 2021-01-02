884e573c69bed169cdc59e506006ed364deac098
Jamal Everett <51199922+jamaleverett@users.noreply.github.com>
2021-01-02 07:49:23 -0800

    Update ig_follower_bot.py
    updated script to reflect webdriver.Chrome()

--- ig_follower_bot.py
+++ ig_follower_bot.py
@@ -3,15 +3,13 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class InstagramBot():
-    def __init__(self,email, password):
-        self.browserProfile = webdriver.ChromeOptions()
-        self.browserProfile.add_experimental_option('prefs', {'intl.accept_languages': 'en,en_US'})
-        self.browser = webdriver.Chrome('chromedriver.exe', chrome_options=self.browserProfile)
+   def __init__(self, email, password):
+        self.browser = webdriver.Chrome()
         self.email = email
         self.password = password
 
-    def signIN(self):
-        self.browser.get('htts://www.instagram.com/accounts/login/')
+    def signIn(self):
+       self.browser.get('https://www.instagram.com/accounts/login/')
 
         emailInput = self.browser.find_elements_by_css_selector('form input') [0]
         passwordInput = self.browser.find_elements_by_css_selector('form input')[1]
